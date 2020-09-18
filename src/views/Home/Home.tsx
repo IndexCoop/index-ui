95a5ab2b6bc70d0fb4140240802e18fef7969192
clintonbembryjr <clintonbembryjr@gmail.com>
2020-09-18 09:29:17 -0700

    hide notices

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -9,12 +9,17 @@ import {
 import Page from 'components/Page'
 import PageHeader from 'components/PageHeader'
 
+import useBalances from 'hooks/useBalances'
+import useVesting from 'hooks/useVesting'
+
 import MigrationNotice from './components/MigrationNotice'
 import Rebase from './components/Rebase'
 import Stats from './components/Stats'
 import VestingNotice from './components/VestingNotice'
 
 const Home: React.FC = () => {
+  const { yamV2Balance } = useBalances()
+  const { vestedBalance } = useVesting()
   return (
     <Page>
       <PageHeader
@@ -23,10 +28,18 @@ const Home: React.FC = () => {
         title="Welcome to YAM Finance."
       />
       <Container>
-        <MigrationNotice />
-        <Spacer size="sm" />
-        <VestingNotice />
-        <Spacer />
+        {(yamV2Balance && yamV2Balance.toNumber() > 0) && (
+          <>
+            <MigrationNotice />
+            <Spacer size="sm" />
+          </>
+        )}
+        {(vestedBalance && vestedBalance.toNumber() > 0) && (
+          <>
+            <VestingNotice />
+            <Spacer />
+          </>
+        )}
         <Box row>
           <Box flex={1}>
             <Rebase />
