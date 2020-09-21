6f72e1f722c4203de7f277191296f985dba5adca
clintonbembryjr <clintonbembryjr@gmail.com>
2020-09-20 23:33:03 -0700

    rebase me

--- src/views/Home/components/Rebase.tsx
+++ src/views/Home/components/Rebase.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+
+import Countdown, { CountdownRenderProps} from 'react-countdown'
 import {
   Box,
   Button,
@@ -6,10 +8,46 @@ import {
   CardContent,
   Spacer,
 } from 'react-neu'
+import styled from 'styled-components'
 
 import Dial from 'components/Dial'
+import Label from 'components/Label'
+
+import useYam from 'hooks/useYam'
+
+import { getNextRebaseTimestamp } from 'yam-sdk/utils'
 
 const Rebase: React.FC = () => {
+  const yam = useYam()
+  const [nextRebase, setNextRebase] = useState<number>()
+
+  const fetchNextRebase = useCallback( async() => {
+    if (!yam) return
+    const nextRebaseTimestamp = await getNextRebaseTimestamp(yam)
+    setNextRebase(nextRebaseTimestamp)
+  }, [
+    setNextRebase,
+    yam
+  ])
+
+  useEffect(() => {
+    if (yam) {
+      fetchNextRebase()
+    }
+  }, [fetchNextRebase, yam])
+
+  const renderer = (countdownProps: CountdownRenderProps) => {
+    const { hours, minutes, seconds } = countdownProps
+    const paddedSeconds = seconds < 10 ? `0${seconds}` : seconds
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes
+    const paddedHours = hours < 10 ? `0${hours}` : hours
+    return (
+      <span>{paddedHours}:{paddedMinutes}:{paddedSeconds}</span>
+    )
+  }
+
+  const dialValue = nextRebase ? nextRebase / (1000 * 60 * 60 * 12) * 100 : 0
+
   return (
     <Card>
       <CardContent>
@@ -18,7 +56,15 @@ const Rebase: React.FC = () => {
           justifyContent="center"
           row
         >
-          <Dial size={240} value={0}>
+          <Dial size={240} value={dialValue}>
+            <StyledCountdown>
+              <StyledCountdownText>
+                {!nextRebase ? '--' : (
+                  <Countdown date={new Date(Date.now() + nextRebase)} renderer={renderer} />
+                )}
+              </StyledCountdownText>
+              <Label text="Next rebase" />
+            </StyledCountdown>
           </Dial>
         </Box>
         <Spacer />
@@ -27,5 +73,16 @@ const Rebase: React.FC = () => {
     </Card>
   )
 }
+
+const StyledCountdown = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`
+const StyledCountdownText = styled.span`
+  color: ${props => props.theme.colors.primary.main};
+  font-size: 36px;
+  font-weight: 700;
+`
 
 export default Rebase
