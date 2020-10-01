fdc154ec62764801cb21fd62872234b3db026adc
Dylan Tran <dylan.ad.tran@gmail.com>
2020-09-30 17:27:58 -0700

    add more transaction states to confirmation modal

--- src/components/ConfirmTransactionModal/ConfirmTransactionModal.tsx
+++ src/components/ConfirmTransactionModal/ConfirmTransactionModal.tsx
@@ -11,8 +11,17 @@ import { useWallet } from 'use-wallet'
 import metamaskLogo from 'assets/metamask-fox.svg'
 import walletConnectLogo from 'assets/wallet-connect.svg'
 
-const ConfirmTransactionModal: React.FC<ModalProps> = ({
+interface ConfirmationModalProps extends ModalProps {
+  isPending?: boolean
+  isCompleted?: boolean
+  isFailed?: boolean
+}
+
+const ConfirmTransactionModal: React.FC<ConfirmationModalProps> = ({
   isOpen,
+  isPending,
+  isCompleted,
+  isFailed,
 }) => {
   const { connector } = useWallet()
 
@@ -23,6 +32,42 @@ const ConfirmTransactionModal: React.FC<ModalProps> = ({
       return <img src={walletConnectLogo} height={72} />
     }
   }, [connector])
+
+  if (isFailed) {
+    return (
+      <Modal isOpen={isOpen}>
+        <ModalContent>
+          {WalletLogo}
+          <Spacer />
+          <StyledText>Your transaction could not be processed.</StyledText>
+        </ModalContent>
+      </Modal>
+    )
+  }
+
+  if (isCompleted) {
+    return (
+      <Modal isOpen={isOpen}>
+        <ModalContent>
+          {WalletLogo}
+          <Spacer />
+          <StyledText>Your transaction succeeded.</StyledText>
+        </ModalContent>
+      </Modal>
+    )
+  }
+
+  if (isPending) {
+    return (
+      <Modal isOpen={isOpen}>
+        <ModalContent>
+          {WalletLogo}
+          <Spacer />
+          <StyledText>Your transaction is being processed.</StyledText>
+        </ModalContent>
+      </Modal>
+    )
+  }
 
   return (
     <Modal isOpen={isOpen}>
