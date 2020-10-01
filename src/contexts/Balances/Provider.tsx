321e6908efd7b4345958f6eb701e9b4b505e55c6
Dylan Tran <dylan.ad.tran@gmail.com>
2020-10-01 15:45:41 -0700

    Display Account Balances (#15)
    * add index, dpi, lp token addresses
    
    * bugfix fetching balances
    
    * display new balances in wallet modal
    
    * fix airdrop quantities
    
    * lint
    
    * revert env

--- src/contexts/Balances/Provider.tsx
+++ src/contexts/Balances/Provider.tsx
@@ -4,52 +4,67 @@ import { useWallet } from 'use-wallet'
 import { provider } from 'web3-core'
 
 import Context from './Context'
+import { getBalance } from 'utils/index'
+import { dpiTokenAddress, indexTokenAddress, uniswapEthDpiLpTokenAddress } from 'constants/tokenAddresses';
 
 const Provider: React.FC = ({ children }) => {
-  const [yamV2Balance, setYamV2Balance] = useState<BigNumber>()
-  const [yamV3Balance, setYamV3Balance] = useState<BigNumber>()
-  const [yycrvUniLpBalance, setYycrvUniLpBalance] = useState<BigNumber>()
+  const [indexBalance, setIndexBalance] = useState<BigNumber>()
+  const [dpiBalance, setDpiBalance] = useState<BigNumber>()
+  const [uniswapEthDpiLpBalance, setUniswapEthDpiLpBalance] = useState<
+    BigNumber
+  >()
 
-  const { account, ethereum }: { account: string | null, ethereum: provider } = useWallet()
+  const {
+    account,
+    ethereum,
+  }: { account: string | null; ethereum: provider } = useWallet()
 
-  const fetchBalances = useCallback(async (userAddress: string, provider: provider) => {
-    setYamV2Balance(new BigNumber(0).dividedBy(new BigNumber(10).pow(24)))
-    setYamV3Balance(new BigNumber(0).dividedBy(new BigNumber(10).pow(18)))
-    setYycrvUniLpBalance(new BigNumber(0).dividedBy(new BigNumber(10).pow(18)))
-  }, [
-    setYamV2Balance,
-    setYamV3Balance,
-    setYycrvUniLpBalance
-  ])
+  const fetchBalances = useCallback(
+    async (userAddress: string, provider: provider) => {
+      const balances = await Promise.all([
+        getBalance(provider, indexTokenAddress as string, userAddress),
+        getBalance(provider, dpiTokenAddress as string, userAddress),
+        getBalance(provider, uniswapEthDpiLpTokenAddress as string, userAddress),
+      ])
+
+      setIndexBalance(
+        new BigNumber(balances[0]).dividedBy(new BigNumber(10).pow(18))
+      )
+      setDpiBalance(
+        new BigNumber(balances[1]).dividedBy(new BigNumber(10).pow(18))
+      )
+      setUniswapEthDpiLpBalance(
+        new BigNumber(balances[2]).dividedBy(new BigNumber(10).pow(18))
+      )
+    },
+    [setIndexBalance, setDpiBalance, setUniswapEthDpiLpBalance,]
+  )
 
   useEffect(() => {
     if (account && ethereum) {
       fetchBalances(account, ethereum)
     }
-  }, [
-    account,
-    ethereum,
-    fetchBalances,
-  ])
+  }, [account, ethereum, fetchBalances])
 
   useEffect(() => {
     if (account && ethereum) {
       fetchBalances(account, ethereum)
-      let refreshInterval = setInterval(() => fetchBalances(account, ethereum), 10000)
+      let refreshInterval = setInterval(
+        () => fetchBalances(account, ethereum),
+        10000
+      )
       return () => clearInterval(refreshInterval)
     }
-  }, [
-    account,
-    ethereum,
-    fetchBalances,
-  ])
+  }, [account, ethereum, fetchBalances])
 
   return (
-    <Context.Provider value={{
-      yamV2Balance,
-      yamV3Balance,
-      yycrvUniLpBalance,
-    }}>
+    <Context.Provider
+      value={{
+        indexBalance,
+        dpiBalance,
+        uniswapEthDpiLpBalance,
+      }}
+    >
       {children}
     </Context.Provider>
   )
