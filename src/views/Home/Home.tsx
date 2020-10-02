7575eab86a6f4ced93b6a59fae3898f9c96308b0
Inje Yeo <byeo630@gmail.com>
2020-10-02 02:39:47 -0700

    Timer: Add countdown timer until launch

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Button, Spacer } from 'react-neu'
+import { Box, Button, Container, Spacer } from 'react-neu'
 
 import Page from 'components/Page'
 import PageHeader from 'components/PageHeader'
 import Explanation from 'components/Explanation'
 
-import Treasury from './components/Treasury'
 import DeFiPulseIndex from './components/DeFiPulseIndex'
+import FarmingTimer from './components/FarmingTimer'
+import Treasury from './components/Treasury'
 
 const Home: React.FC = () => {
 
@@ -17,14 +18,20 @@ const Home: React.FC = () => {
         title='Index DAO'
         subtitle='A global community launching and maintaining the world’s best crypto index vehicles'
       />
-      <Button text='View Farms' to='/farm' />
-      <Spacer />
-      <Treasury />
-      <Spacer />
-      <Explanation />
-      <Spacer />
-      <DeFiPulseIndex />
-      <Spacer />
+        <Button text='View Farms' to='/farm' />
+        <Spacer />
+        <Treasury />
+        <Spacer />
+        <Container>
+          <Box row justifyContent="center">
+            <FarmingTimer />
+            <Spacer />
+            <Explanation />
+          </Box>
+        </Container>
+        <Spacer />
+        <DeFiPulseIndex />
+        <Spacer />
     </Page>
   )
 }
