d85f834ea245bea22734ba420e02fd4962dcb4f4
Inje Yeo <byeo630@gmail.com>
2020-10-08 13:11:26 -0700

    Remove second localStorage wallet connect removal

--- src/contexts/Wallet/WalletProvider.tsx
+++ src/contexts/Wallet/WalletProvider.tsx
@@ -17,7 +17,6 @@ const WalletProvider: React.FC = ({ children}) => {
     if (active) deactivate()
 
     setConnector('');
-    localStorage.removeItem('walletconnect');
     setStatus('disconnected');
     localStorage.removeItem('walletconnect');
   }, [account, active, connector, status]);
