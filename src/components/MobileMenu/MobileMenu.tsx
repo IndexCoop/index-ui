e5317906af8be1d92582102eadf098af6338ec36
Dylan Tran <dylan.ad.tran@gmail.com>
2020-10-30 10:58:40 -0700

    add index, dpi to mobile nav menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -4,7 +4,7 @@ import styled, { keyframes } from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
 interface MobileMenuProps {
-  onDismiss: () => void,
+  onDismiss: () => void
   visible?: boolean
 }
 
@@ -14,9 +14,26 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ onDismiss, visible }) => {
       <StyledMobileMenuWrapper>
         <StyledBackdrop onClick={onDismiss} />
         <StyledMobileMenu>
-          <StyledLink exact activeClassName="active" to="/" onClick={onDismiss}>Home</StyledLink>
-          <StyledLink exact activeClassName="active" to="/farm" onClick={onDismiss}>Farm</StyledLink>
-          <StyledLink activeClassName="active" to="/faq" onClick={onDismiss}>FAQ</StyledLink>
+          <StyledLink exact activeClassName='active' to='/' onClick={onDismiss}>
+            Home
+          </StyledLink>
+          <StyledLink
+            exact
+            activeClassName='active'
+            to='/farm'
+            onClick={onDismiss}
+          >
+            Farm
+          </StyledLink>
+          <StyledLink exact activeClassName='active' to='/dpi'>
+            DeFi Pulse Index
+          </StyledLink>
+          <StyledLink exact activeClassName='active' to='/index'>
+            INDEX
+          </StyledLink>
+          <StyledLink activeClassName='active' to='/faq' onClick={onDismiss}>
+            FAQ
+          </StyledLink>
         </StyledMobileMenu>
       </StyledMobileMenuWrapper>
     )
@@ -25,17 +42,23 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ onDismiss, visible }) => {
 }
 
 const StyledBackdrop = styled.div`
-  background-color: ${props => props.theme.colors.black};
+  background-color: ${(props) => props.theme.colors.black};
   opacity: 0.75;
   position: absolute;
-  top: 0; right: 0; bottom: 0; left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 `
 
 const StyledMobileMenuWrapper = styled.div`
   display: flex;
   flex-direction: column;
   position: fixed;
-  top: 0; right: 0; bottom: 0; left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   z-index: 1000;
 `
 
@@ -50,30 +73,33 @@ const slideIn = keyframes`
 
 const StyledMobileMenu = styled.div`
   animation: ${slideIn} 0.3s forwards ease-out;
-  background: ${props => props.theme.baseBg};
+  background: ${(props) => props.theme.baseBg};
   display: flex;
   flex: 1;
   flex-direction: column;
   justify-content: center;
   position: absolute;
-  top: 0; left: 100%; bottom: 0;
+  top: 0;
+  left: 100%;
+  bottom: 0;
   width: calc(100% - 48px);
 `
 
 const StyledLink = styled(NavLink)`
   box-sizing: border-box;
-  color: ${props => props.theme.colors.grey[500]};
+  color: ${(props) => props.theme.colors.grey[500]};
   font-size: 24px;
   font-weight: 700;
-  padding: ${props => props.theme.spacing[3]}px ${props => props.theme.spacing[4]}px;
+  padding: ${(props) => props.theme.spacing[3]}px
+    ${(props) => props.theme.spacing[4]}px;
   text-align: center;
   text-decoration: none;
   width: 100%;
   &:hover {
-    color: ${props => props.theme.colors.grey[600]};
+    color: ${(props) => props.theme.colors.grey[600]};
   }
   &.active {
-    color: ${props => props.theme.colors.primary.main};
+    color: ${(props) => props.theme.colors.primary.main};
   }
 `
 
