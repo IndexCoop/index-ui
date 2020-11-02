fb99b5fc81a99fbc4f6ebc385825818710eace74
Dylan Tran <dylan.ad.tran@gmail.com>
2020-11-02 11:08:37 -0800

    init state in buy/sell provider

--- src/contexts/BuySell/BuySellProvider.tsx
+++ src/contexts/BuySell/BuySellProvider.tsx
@@ -3,8 +3,29 @@ import React, { useState, useEffect } from 'react'
 import BuySellContext from './BuySellContext'
 
 const BuySellProvider: React.FC = ({ children }) => {
+  const [isViewingOrderSummary] = useState<boolean>(false)
+  const [isFetchingOrderData] = useState<boolean>(false)
+  const [activeField] = useState<'currency' | 'token'>('currency')
+  const [selectedCurrency] = useState<any>()
+  const [currencyQuantity] = useState<number>(0)
+  const [tokenQuantity] = useState<number>(0)
+  const [currencyOptions] = useState<any[]>([])
+  const [uniswapData] = useState<any>({})
+
   return (
-    <BuySellContext.Provider value={{ onExecuteBuySell: () => {} }}>
+    <BuySellContext.Provider
+      value={{
+        isViewingOrderSummary,
+        isFetchingOrderData,
+        activeField,
+        selectedCurrency,
+        currencyQuantity,
+        tokenQuantity,
+        currencyOptions,
+        uniswapData,
+        onExecuteBuySell: () => {},
+      }}
+    >
       {children}
     </BuySellContext.Provider>
   )
