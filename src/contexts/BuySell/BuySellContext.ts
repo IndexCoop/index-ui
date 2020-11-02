727088be230de29aadb00755869af973cdd05d43
Dylan Tran <dylan.ad.tran@gmail.com>
2020-11-02 11:03:01 -0800

    init. buy sell context state

--- src/contexts/BuySell/BuySellContext.ts
+++ src/contexts/BuySell/BuySellContext.ts
@@ -1,10 +1,26 @@
 import { createContext } from 'react'
 
 interface BuySellContextValues {
+  isViewingOrderSummary: boolean
+  isFetchingOrderData: boolean
+  activeField: 'currency' | 'token'
+  selectedCurrency: undefined
+  currencyQuantity: number
+  tokenQuantity: number
+  currencyOptions: any[]
+  uniswapData: any
   onExecuteBuySell: () => void
 }
 
 const BuySellContext = createContext<BuySellContextValues>({
+  isViewingOrderSummary: false,
+  isFetchingOrderData: false,
+  activeField: 'currency',
+  selectedCurrency: undefined,
+  currencyQuantity: 0,
+  tokenQuantity: 0,
+  currencyOptions: [],
+  uniswapData: {},
   onExecuteBuySell: () => {},
 })
 
