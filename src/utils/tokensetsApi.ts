577eb24df46c54003f30f612f8d0a8461922f959
Dylan Tran <dylan.ad.tran@gmail.com>
2020-11-05 14:17:57 -0800

    setup token sets api util

---
+++ src/utils/tokensetsApi.ts
@@ -0,0 +1,19 @@
+const baseURL = 'https://api.tokensets.com/public'
+
+export const fetchTokenBuySellData = (
+  id: string,
+  isBuyOrder: boolean,
+  requestQuantity: number | string,
+  currencyId: string,
+  activeField: 'token' | 'currency'
+) => {
+  const buyOrSellRoute = isBuyOrder ? 'buy_price' : 'sell_price'
+  const requestUrl = `${baseURL}/v2/portfolios/${id}/${buyOrSellRoute}?quantity=${requestQuantity}&currency=${currencyId}&input_type=${activeField}`
+
+  return fetch(requestUrl)
+    .then((response) => {
+      console.log('response is', response)
+      return response
+    })
+    .catch((error) => console.log(error))
+}
