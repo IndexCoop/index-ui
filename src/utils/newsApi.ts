135a08bed33d38f70d9d419d8b54e0441dd886cc
Dylan Tran <dylan.ad.tran@gmail.com>
2020-11-18 14:13:26 -0800

    add more articles to news section

--- src/utils/newsApi.ts
+++ src/utils/newsApi.ts
@@ -1,5 +1,12 @@
 const newsContent = [
   {
+    image: 'https://miro.medium.com/max/1400/0*c1iwaImJm9AYc4iw',
+    title: 'View From the Nest #2 (Weekly Summary)',
+    author: 'DarkForestCapital',
+    readTime: '5 minute read',
+    link: 'https://indexcoop.substack.com/p/view-from-the-nest-2',
+  },
+  {
     image: 'https://miro.medium.com/max/1400/1*jGrGl4jcZ_uUQZ3xDM-Ntw.png',
     title: 'Index Coop October Recap',
     author: 'Kiba',
@@ -7,12 +14,34 @@ const newsContent = [
     link: 'https://medium.com/indexcoop/index-coop-october-update-d6e004136bb5',
   },
   {
-    image: 'https://miro.medium.com/max/1400/0*c1iwaImJm9AYc4iw',
+    image:
+      'https://pbs.twimg.com/profile_banners/1312497976883904512/1602012492/1500x500',
     title: 'The Top Secret, Open Source, Composable, Master Plan',
     author: 'Overanalyser',
     readTime: '5 minute read',
     link:
       'https://overanalyser.medium.com/the-top-secret-open-source-composable-master-plan-5957b974574a',
+  },
+  {
+    image: 'http://i3.ytimg.com/vi/H64oDYVoffQ/maxresdefault.jpg',
+    title: 'Talking Crypto With Over Analyser',
+    author: 'Gabriel Haines',
+    readTime: '1 hour podcast',
+    link: 'https://www.youtube.com/watch?v=H64oDYVoffQ&feature=youtu.be',
+  },
+  {
+    image: 'https://miro.medium.com/max/1400/1*eJwOjiCrBvKIl-1SEp8s6Q.png',
+    title: 'Rebalancing $DPI',
+    author: 'Overanalyser',
+    readTime: '4 minute read',
+    link: 'https://overanalyser.medium.com/rebalancing-dpi-b074a792bc70',
+  },
+  {
+    image: 'https://miro.medium.com/max/1400/0*c1iwaImJm9AYc4iw',
+    title: 'View From the Nest #1 (Weekly Summary)',
+    author: 'DarkForestCapital',
+    readTime: '5 minute read',
+    link: 'https://indexcoop.substack.com/p/view-from-the-nest-1',
   },
   {
     image:
