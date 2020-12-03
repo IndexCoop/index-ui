455d3f6d7e8e00a41f4de34e7aa743c1d6ba277f
Dylan Tran <dylan.ad.tran@gmail.com>
2020-12-03 14:04:48 -0800

    replace farm header

--- src/views/Farm/Farm.tsx
+++ src/views/Farm/Farm.tsx
@@ -1,18 +1,11 @@
 import React, { useMemo } from 'react'
-
-import {
-  Box,
-  Button,
-  Container,
-  Separator,
-  Spacer,
-} from 'react-neu'
+import styled from 'styled-components'
+import { Box, Button, Container, Separator, Spacer } from 'react-neu'
 
 import useFarming from 'hooks/useFarming'
 import useWallet from 'hooks/useWallet'
 
 import Page from 'components/Page'
-import PageHeader from 'components/PageHeader'
 import Split from 'components/Split'
 
 import HarvestCard from './components/Harvest'
@@ -26,38 +19,31 @@ const Farm: React.FC = () => {
 
   const UnstakeAndHarvestButton = useMemo(() => {
     if (status !== 'connected') {
-      return (
-        <Button
-          disabled
-          text="Claim &amp; Unstake"
-          variant="secondary"
-        />
-      )
+      return <Button disabled text='Claim &amp; Unstake' variant='secondary' />
     }
     return (
       <Button
         onClick={onUnstakeAndHarvest}
-        text="Claim &amp; Unstake"
-        variant="secondary"
+        text='Claim &amp; Unstake'
+        variant='secondary'
       />
     )
-  }, [
-    status,
-    onUnstakeAndHarvest,
-  ])
+  }, [status, onUnstakeAndHarvest])
   const icon = {
     src: 'https://index-dao.s3.amazonaws.com/moon.png',
     alt: 'Moon',
-  };
+  }
 
   return (
     <Page>
-      <PageHeader
-        icon={icon}
-        subtitle="Stake ETH/DPI LP tokens and farm INDEX"
-        title="Farm"
-      />
-      <Spacer size="md" />
+      <Container>
+        <StyledPageHeader>Farm</StyledPageHeader>
+        <Spacer size='sm' />
+        <StyledPageSubheader>
+          Earn rewards for supplying DPI liquidity
+        </StyledPageSubheader>
+      </Container>
+      <Spacer />
       <Treasury />
       <Spacer />
       <Container>
@@ -66,19 +52,32 @@ const Farm: React.FC = () => {
           <HarvestCard />
         </Split>
         <Spacer />
-        <Box row justifyContent="center">
+        <Box row justifyContent='center'>
           {UnstakeAndHarvestButton}
         </Box>
-        <Spacer size="lg" />
+        <Spacer size='lg' />
         <Separator />
-        <Spacer size="lg" />
-        <Box row justifyContent="center">
+        <Spacer size='lg' />
+        <Box row justifyContent='center'>
           <RewardsCard />
         </Box>
-        <Spacer size="lg" />
+        <Spacer size='lg' />
       </Container>
     </Page>
   )
 }
+
+const StyledPageHeader = styled.div`
+  color: ${(props) => props.theme.textColor};
+  font-size: 48px;
+  font-weight: 700;
+  width: 100%;
+`
+
+const StyledPageSubheader = styled.div`
+  color: ${(props) => props.theme.colors.grey[500]};
+  font-size: 24px;
+  width: 100%;
+`
 
 export default Farm
