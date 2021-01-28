c56d6e874141f05c12f6b467ed9f0e4ca9818e82
ncitron <noah@jeff.org>
2021-01-27 23:23:23 -0500

    added news fetch from google sheets

--- src/utils/newsApi.ts
+++ src/utils/newsApi.ts
@@ -1,3 +1,5 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+
 const newsContent = [
   {
     image:
@@ -113,8 +115,25 @@ const newsContent = [
   },
 ]
 
-const fetchNews = () => {
-  return newsContent
+const fetchNews = async () => {
+  const API_KEY = process.env.REACT_APP_GOOGLE_SEEHTS_API_KEY
+  const SHEET_ID = process.env.REACT_APP_NEWS_GOOGLE_SHEET_ID
+  const doc = new GoogleSpreadsheet(SHEET_ID)
+  doc.useApiKey(API_KEY)
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0]
+
+  let rows = await sheet.getRows()
+  let news = rows.map((row: any) => {
+    return {
+      image: row['image'],
+      title: row['title'],
+      author: row['author'],
+      readTime: row['readTime'],
+      link: row['link'],
+    }
+  })
+  return news.reverse()
 }
 
 export default fetchNews
