2d3f55998dd555c974aae4c27c86bf22be894412
controtie <dylan.ad.tran@gmail.com>
2021-02-07 23:43:47 -0800

    remove GBP as default cryptocurrency

--- src/components/TransakBuySellButton/TransakBuySellButton.tsx
+++ src/components/TransakBuySellButton/TransakBuySellButton.tsx
@@ -11,8 +11,8 @@ const getTransakLauncher = (isMobile: boolean, buySellToken: string) => {
     apiKey: process.env.REACT_APP_TRANSAK_API_KEY,
     environment: process.env.REACT_APP_ENVIRONMENT?.toUpperCase(), // STAGING or PRODUCTION
     defaultCryptoCurrency: buySellToken,
-    fiatCurrency: 'GBP',
     themeColor: '0063ed',
+    // fiatCurrency: 'GBP',
     // redirectURL: 'http://localhost:3000',
     hostURL: window.location.origin,
     widgetHeight: '550px',
