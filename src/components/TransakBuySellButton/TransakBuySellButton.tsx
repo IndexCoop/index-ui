8b607e311691c886c0d0cd7a640683d841990000
Dylan Tran <dylan.ad.tran@gmail.com>
2021-02-08 14:55:37 -0800

    split up transak launcher into modal

--- src/components/TransakBuySellButton/TransakBuySellButton.tsx
+++ src/components/TransakBuySellButton/TransakBuySellButton.tsx
@@ -1,43 +1,22 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import styled from 'styled-components'
 
 import { RoundedButton } from 'components/RoundedButton'
-import useBuySell from 'hooks/useBuySell'
-import useMediaQuery from 'hooks/useMediaQuery'
-
-const transakSDK = require('@transak/transak-sdk').default
-
-const getTransakLauncher = (isMobile: boolean, buySellToken: string) => {
-  let transak = new transakSDK({
-    apiKey: process.env.REACT_APP_TRANSAK_API_KEY,
-    environment: process.env.REACT_APP_ENVIRONMENT?.toUpperCase(), // STAGING or PRODUCTION
-    defaultCryptoCurrency: buySellToken,
-    themeColor: '0063ed',
-    // fiatCurrency: 'GBP',
-    // redirectURL: 'http://localhost:3000',
-    hostURL: window.location.origin,
-    widgetHeight: '550px',
-    widgetWidth: isMobile ? '350px' : '450px',
-  })
-
-  transak.init()
-
-  transak.on(transak.ALL_EVENTS, (data: any) => {
-    console.log(data)
-  })
-}
+import TransakModal from 'components/TransakModal'
 
 /**
- * TransakBuySellButton - Displays a button used to initiate a Transak buy sell flow
+ * TransakBuySellButton - Displays a button used to open a modal displaying information about Transak order flow.
  */
 const TransakBuySellButton: React.FC = () => {
-  const { buySellToken } = useBuySell()
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  const { isMobile } = useMediaQuery()
+  const onOpenModal = useCallback(() => {
+    setIsModalOpen(true)
+  }, [isModalOpen, setIsModalOpen])
 
-  const onOpenTransakLauncher = useCallback(() => {
-    return getTransakLauncher(isMobile as boolean, buySellToken)
-  }, [isMobile, buySellToken])
+  const onDismissModal = useCallback(() => {
+    setIsModalOpen(false)
+  }, [isModalOpen, setIsModalOpen])
 
   return (
     <StyledButtonWrapper>
@@ -46,8 +25,9 @@ const TransakBuySellButton: React.FC = () => {
         isDisabled={false}
         isPending={false}
         text='Buy with Cash'
-        onClick={onOpenTransakLauncher}
+        onClick={onOpenModal}
       />
+      <TransakModal isOpen={isModalOpen} onDismiss={onDismissModal} />
     </StyledButtonWrapper>
   )
 }
