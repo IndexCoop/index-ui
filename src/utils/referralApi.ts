7de45f76cfe1c902f01f3287f8ac167545e70d5e
Dylan Tran <dylan.ad.tran@gmail.com>
2021-02-23 12:55:46 -0800

    read credentials via env variable

--- src/utils/referralApi.ts
+++ src/utils/referralApi.ts
@@ -1,12 +1,14 @@
-import cred from 'serviceAccount.json'
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 
 const addReferral = async (transactionId: string, referral: string) => {
-  const SHEET_ID = process.env.REACT_APP_REFERRAL_GOOGLE_SHEET_ID
-  const doc = new GoogleSpreadsheet(SHEET_ID)
-
   try {
-    doc.useServiceAccountAuth(cred)
+    const credentials = JSON.parse(
+      process.env.REACT_APP_REFERRAL_GOOGLE_API_KEY!
+    )
+    const SHEET_ID = process.env.REACT_APP_REFERRAL_GOOGLE_SHEET_ID
+    const doc = new GoogleSpreadsheet(SHEET_ID)
+
+    doc.useServiceAccountAuth(credentials)
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[0]
     await sheet.addRow({
