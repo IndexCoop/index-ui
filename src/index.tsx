c0738b87111a2eac4303f4634509616abb9c8cd7
ztcrypto <tony.zhou.vue@gmail.com>
2021-03-04 11:58:29 -0800

    restore yarn.lock and index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,12 @@ import gtmConfig from './utils/googleTagManager'
 
 TagManager.initialize(gtmConfig)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
