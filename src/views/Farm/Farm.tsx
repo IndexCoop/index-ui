72f5e8cd1cded8665e40dfa82d99236c39e97812
Dylan Tran <dylan.ad.tran@gmail.com>
2021-04-05 23:01:01 -0700

    update naming on farm cards

--- src/views/Farm/Farm.tsx
+++ src/views/Farm/Farm.tsx
@@ -5,8 +5,8 @@ import { Box, Container, Separator, Spacer } from 'react-neu'
 import Page from 'components/Page'
 import Split from 'components/Split'
 
-import StakeCard from './components/Stake'
-import StakeFarmTwoCard from './components/Stake/StakeFarmTwo'
+import LegacyFarmCard from './components/Stake/LegacyFarm'
+import DpiFarmCard from './components/Stake/DpiFarm'
 import RewardsCard from './components/Rewards'
 import Treasury from './components/Treasury'
 import MigrationNotice from './components/MigrationNotice'
@@ -19,10 +19,10 @@ const Farm = (props: { title: string }) => {
   return (
     <Page>
       <Container>
-        <StyledPageHeader>Farm</StyledPageHeader>
+        <StyledPageHeader>Liquidity Mining Programs</StyledPageHeader>
         <Spacer size='sm' />
         <StyledPageSubheader>
-          Earn rewards for supplying DPI liquidity
+          Earn rewards for supplying liquidity for Index Coop products
         </StyledPageSubheader>
         <Spacer />
         <MigrationNotice />
@@ -31,12 +31,18 @@ const Farm = (props: { title: string }) => {
       <Spacer />
       <Container>
         <Split>
-          <StakeCard />
-          <StakeFarmTwoCard />
+          <LegacyFarmCard />
+          <DpiFarmCard />
         </Split>
         <Spacer />
         <Separator />
         <Spacer size='lg' />
+        <StyledPageHeader>Index Airdrop</StyledPageHeader>
+        <Spacer size='sm' />
+        <StyledPageSubheader>
+          See if you qualify for the Index Airdrop
+        </StyledPageSubheader>
+        <Spacer />
         <Box row justifyContent='center'>
           <RewardsCard />
         </Box>
