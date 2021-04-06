8c844fdd31a19d089fb9b6ce2e41e868a4ef0d0b
Dylan Tran <dylan.ad.tran@gmail.com>
2021-04-05 20:16:57 -0700

    update mvi description

--- src/views/MVI/MVI.tsx
+++ src/views/MVI/MVI.tsx
@@ -49,115 +49,79 @@ const MviProductPage = (props: { title: string }) => {
           /> */}
           <Description>
             <p>
-              <strong>
-                The CoinShares Gold and Cryptoassets Index Lite (CGI)
-              </strong>{' '}
-              is a low-volatility index that utilises the concept of volatility
-              harvesting through (a) forming an equally-weighted basket of
-              cryptoassets ($BTC and $ETH) and (b) combining it with gold using
-              a weighted-risk contribution as a rebalancing mechanism. By
-              decreasing volatility levels, the CGI seeks to yield superior
-              risk-adjusted returns when compared to a number of alternative
-              strategies, including holding cryptoassets or gold alone. Further,
-              it presents a moderate turnover, which should translate into
-              moderate operating costs.
-            </p>
-            <p>
-              The CGI is an adaptation of the CoinShares Gold and Cryptoassets
-              Index (CGCI) that was launched by CoinShares in 2020 and is one of
-              the first EU Benchmark Regulated (EU BMR) indices for
-              cryptoassets.
-            </p>
-            <h2>Objective</h2>
-            <p>
-              The CGI is designed with the aim of providing diversified exposure
-              to the alternative asset space in a way that yields a superior
-              risk-return profile when compared to holding such crypto and gold
-              in isolation.
-            </p>
-            <p>
-              Two noteworthy characteristics of the returns of cryptoassets are
-              their high volatility, which brings with it a high level of risk,
-              and their high intraclass correlation, which limits the benefits
-              that can be had by diversifying across multiple cryptoassets. Yet
-              cryptoassets exhibit no correlation with gold, a highly-liquid yet
-              scarce asset which has proved to function as a safe haven during
-              crises affecting traditional financial systems. Although
-              volatility poses challenges in terms of increased uncertainty,
-              there are also benefits to be had from its proper management
-              through diversification and regular rebalancing. This is
-              exemplified by the so-called Shannon’s Demon approach in which
-              two, ideally uncorrelated, assets – at least one of which is
-              highly volatile – are periodically rebalanced to maintain an ideal
-              target allocation. The resulting expected growth rate is greater
-              than the arithmetic mean of the individual expected growth rates,
-              while the variance of the returns is less than the mean of the
-              individual variances.
-            </p>
-            <h2>Constituent Weighting</h2>
-            <p>
-              The CGI follows a bi-level approach that involves studying the
-              historical volatilities of the crypto-basket ($BTC and $ETH) and
-              gold ($DGLD) separately in order to inform the crypto-gold asset
-              allocation decision.
-            </p>
-            <p>
-              The CGI is calculated following a two-stage allocation scheme that
-              involves:
-            </p>
-            <ul>
-              <li>
-                Computation of the historical volatility of the equally-weighted
-                crypto-basket and gold.
-              </li>
-              <li>
-                Asset allocation between the crypto-basket and gold is expressed
-                using the bivariate weighted risk contribution problem. The risk
-                contribution ratio is set as α = 9, indicating that 90% of the
-                total risk emanates from the crypto-basket. This accounts for a
-                proper risk contribution and delivers a risk and return profile
-                that is superior to holding gold or cryptoassets alone.
-              </li>
-            </ul>
-            <h2>Rebalancing Mechanism</h2>
-            <p>
-              In order to fully capture the diversification benefits of the time
-              varying correlations between gold and the cryptobasket, a monthly
-              rebalancing frequency is employed. Constituent weights are
-              announced on the last business day of the running month. The
-              rebalancing is employed on the first business day of the following
-              month.
-            </p>
-            <h2>Index Calculation</h2>
-            <p>
-              The index level is calculated as described in section 2 of the
-              methodology available{' '}
-              {
-                <ExternalLink
-                  href='https://coinshares.com/assets/resources/cgi-methodology.pdf'
-                  target='_blank'
-                >
-                  here.
-                </ExternalLink>
-              }
-            </p>
-
-            <h2>Data Handling</h2>
-            <div>
-              The following rounding of data are used for the Index calculation
-              and publication:
+              <strong>The Metaverse Index (MVI).</strong>{' '}
+              <h2>Constituent Weighting</h2>
+              Selection of the $MVI tokens would be based on the following basic
+              criteria:
               <ul>
-                <li>Prices are rounded to eight significant figures</li>
-                <li>Weights are rounded to four decimal places</li>
-                <li>Index levels are rounded to two decimal places</li>
+                <li>The token must be available on the Ethereum blockchain.</li>
+                <li>
+                  Protocol must be in one of the following token categories on
+                  Coingecko: Non Fungible Tokens, Entertainment, Virtual
+                  Reality, Augmented Reality and Music. More categories can be
+                  added in the future as the market matures.
+                </li>
+                <li>Total market cap must be over $30m.</li>
+                <li>
+                  Protocol must have at least 3 months history of operation and
+                  its token must have at least 3 months of price and liquidity
+                  history.
+                </li>
+                <li>
+                  Token must have reasonable and consistent DEX liquidity on
+                  Ethereum.
+                </li>
+                <li>
+                  An independent security audit should have been performed on
+                  the protocol and results reviewed by the product
+                  methodologist. In the case that no audit has been performed,
+                  the methodologist will apply subjective judgement of the
+                  protocol based on assessment of the criteria above and
+                  communications with the team.
+                </li>
+                <li>
+                  In the event of a security issue the methodologist should work
+                  with the project team to understand the issue and any effects
+                  to the $MVI holdings. The team would be expected to provide
+                  users of the protocol with a reliable solution and adequate
+                  documentation to ensure transparency about the incident.
+                </li>
+                <li>
+                  Tokens will not be staked at the launch of the index. This is
+                  subject to change as liquidity increases and it becomes
+                  possible to safely generate yield through staking.
+                </li>
               </ul>
-            </div>
-            <ExternalLink
-              href='https://coinshares.com/assets/resources/cgi-methodology.pdf'
-              target='_blank'
-            >
-              <h4>CoinShares full methodology is accessible here.</h4>
-            </ExternalLink>
+              <h2>Rebalancing Mechanism</h2>
+              <h2>Index Calculation</h2>
+              <p>
+                The $MVI uses a combination of root market cap and liquidity
+                weighting to arrive at the final index weights. We believe that
+                liquidity is an important consideration in this space and should
+                be considered when determining portfolio allocation.
+              </p>
+              <p>TW = 75%*RMCW + 25%*LW</p>
+              <p>where,</p>
+              <p>TW – token weight in the $MVI</p>
+              <p>TW – token weight in the $MVI</p>
+              <p>RMCW – square root of market cap weighted allocation</p>
+              <p>LW – liquidity weighted allocation</p>
+              <h2>Index Maintenance</h2>
+              The index is maintained monthly in two phases:
+              <ul>
+                <li>
+                  Determination phase: During the determination phase, the
+                  tokens being added and deleted from the index calculation are
+                  determined during the final week of the month and published
+                  before monthly rebalancing.
+                </li>
+                <li>
+                  Rebalancing phase: Following publication of the determination
+                  phase outcome, the index composition will change to the new
+                  weights during the first week of the following month.
+                </li>
+              </ul>
+            </p>
           </Description>
         </ProductPageContent>
       </Container>
