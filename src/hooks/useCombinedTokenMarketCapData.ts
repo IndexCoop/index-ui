758f4208b5cf7ecb44f96e0913f12e6117041e5e
0xModene <7647623+0xModene@users.noreply.github.com>
2021-04-08 21:30:14 -0400

    MVI TVL added to homepage (#166)

--- src/hooks/useCombinedTokenMarketCapData.ts
+++ src/hooks/useCombinedTokenMarketCapData.ts
@@ -3,14 +3,16 @@ import { useContext } from 'react'
 import { DpiTokenMarketDataContext } from 'contexts/DpiTokenMarketData'
 import { CgiTokenMarketDataContext } from 'contexts/CgiTokenMarketData'
 import { FliTokenMarketDataContext } from 'contexts/FliTokenMarketData'
+import { MviTokenMarketDataContext } from 'contexts/MviTokenMarketData'
 
 const useCombinedTokenMarketCapData = () => {
   const allMarketCaps = [
     { ...useContext(DpiTokenMarketDataContext) }?.latestMarketCap || 0,
     { ...useContext(CgiTokenMarketDataContext) }?.latestMarketCap || 0,
-    { ...useContext(FliTokenMarketDataContext) }?.latestMarketCap || 0
+    { ...useContext(FliTokenMarketDataContext) }?.latestMarketCap || 0,
+    { ...useContext(MviTokenMarketDataContext) }?.latestMarketCap || 0,
   ]
-  return allMarketCaps?.reduce((total,cap)=> total + cap);
+  return allMarketCaps?.reduce((total, cap) => total + cap)
 }
 
 export default useCombinedTokenMarketCapData
