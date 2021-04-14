a421538b4ebd116959ce2246f1c79ff87d5ac766
Dylan Tran <dylan.ad.tran@gmail.com>
2021-04-13 20:27:46 -0700

    track promo status with local storage

--- src/hooks/usePromoToast.ts
+++ src/hooks/usePromoToast.ts
@@ -1,14 +1,24 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
+import useLocalStorage from './useLocalStorage'
 
-const usePromoToast = (component: React.ReactElement) => {
+const usePromoToast = (component: React.ReactElement, toastId: string) => {
+  const [hasBeenDismissed, setHasBeenDismissed] = useLocalStorage(
+    toastId,
+    false
+  )
+
   useEffect(() => {
-    toast.success(component, {
-      position: 'bottom-right',
-      autoClose: false,
-      closeOnClick: false,
-      closeButton: true,
-    })
+    !hasBeenDismissed &&
+      toast.success(component, {
+        position: 'bottom-right',
+        autoClose: false,
+        closeOnClick: false,
+        draggable: false,
+        closeButton: true,
+        toastId,
+        onClose: () => setHasBeenDismissed(true),
+      })
   }, [])
 
   return null
