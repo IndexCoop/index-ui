5dab55f8da52864e78967c1701979e44abb7f1e4
0xModene <7647623+0xModene@users.noreply.github.com>
2021-05-06 19:50:05 -0500

    april rewards distribution (#210)

--- src/constants/ethContractAddresses.ts
+++ src/constants/ethContractAddresses.ts
@@ -17,6 +17,8 @@ export const february2021RewardsAddress =
   process.env.REACT_APP_FEBRUARY_2021_REWARDS_ADDRESS
 export const march2021RewardsAddress =
   process.env.REACT_APP_MARCH_2021_REWARDS_ADDRESS
+export const april2021RewardsAddress =
+  process.env.REACT_APP_APRIL_2021_REWARDS_ADDRESS
 
 // Token Addresses
 export const indexTokenAddress = process.env.REACT_APP_INDEX_TOKEN_ADDRESS
