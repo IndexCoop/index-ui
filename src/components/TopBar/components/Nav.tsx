fcb0e6a2a33a613efee9f7e2904b9cec9ccfddeb
pouta <moura@hey.com>
2021-05-26 23:39:22 +0100

    Add link to institutional page

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -18,6 +18,12 @@ const Nav: React.FC = () => {
       <StyledLink exact activeClassName='active' to='/vote'>
         Vote
       </StyledLink>
+      <StyledOutboundLink
+        href='https://institutional.indexcoop.com/'
+        target='_blank'
+      >
+        Institutions
+      </StyledOutboundLink>
     </StyledNav>
   )
 }
@@ -38,6 +44,16 @@ const StyledLink = styled(NavLink)`
   }
   &.active {
     color: ${(props) => props.theme.colors.primary.light};
+  }
+`
+const StyledOutboundLink = styled.a`
+  color: ${(props) => props.theme.colors.grey[500]};
+  font-weight: 700;
+  padding-left: ${(props) => props.theme.spacing[3]}px;
+  padding-right: ${(props) => props.theme.spacing[3]}px;
+  text-decoration: none;
+  &:hover {
+    color: ${(props) => props.theme.colors.grey[600]};
   }
 `
 
