c9358d223c584e618cb1860cf65d23dab58c6b62
Noah Citron <noah@jeff.org>
2021-05-28 22:11:49 -0400

    fix snapshot links

--- src/utils/snapshotApi.ts
+++ src/utils/snapshotApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const BASE_URL =
-  process.env.REACT_APP_SNAPSHOT_URL || 'https://hub.snapshot.page/api'
-const INDEX_PROPOSALS_PATH = 'index/proposals'
+  process.env.REACT_APP_SNAPSHOT_URL || 'https://hub.snapshot.org/api'
+const INDEX_PROPOSALS_PATH = 'index-coop.eth/proposals'
 
 export const fetchIndexProposals = () => {
   const indexProposalsUrl = `${BASE_URL}/${INDEX_PROPOSALS_PATH}`
