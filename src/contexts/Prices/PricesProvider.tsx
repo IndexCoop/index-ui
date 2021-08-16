a0087e955f93aa8caa78860a36eec8a59af39f12
Dylan Tran <dylan.ad.tran@gmail.com>
2021-08-16 09:06:27 -0700

    remove rewards for DPI & MVI display (#348)

--- src/contexts/Prices/PricesProvider.tsx
+++ src/contexts/Prices/PricesProvider.tsx
@@ -91,7 +91,7 @@ const PricesProvider: React.FC = ({ children }) => {
     )
       return
 
-    const totalTokenEmissionsPerDay = 425
+    const totalTokenEmissionsPerDay = 0
     const totalUSDEmissionPerDay =
       totalTokenEmissionsPerDay * Number(indexPrice)
 
@@ -130,7 +130,7 @@ const PricesProvider: React.FC = ({ children }) => {
     )
       return
 
-    const totalTokenEmissionsPerDay = 110
+    const totalTokenEmissionsPerDay = 0
     const totalUSDEmissionPerDay =
       totalTokenEmissionsPerDay * Number(indexPrice)
 
