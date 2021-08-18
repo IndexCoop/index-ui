0b8cd07afb937cfa9038a63a807c8b18baa9a0cd
Dylan Tran <dylan.ad.tran@gmail.com>
2021-08-17 21:44:27 -0700

    comment in active farm for kovan testing/deploy

--- src/constants/v3Farms.ts
+++ src/constants/v3Farms.ts
@@ -25,13 +25,13 @@ export const DPIETH: V3Farm = {
     //   refundee: '0xf6401adc23Faa6B9AD83eA8604CA7254CB7F53e7',
     // },
     // // acitve until mid august
-    // {
-    //   rewardToken: '0x1720668a1826c6f30a11780783b0357269b7e1ca',
-    //   pool: '0x1cC56b205109A832Fd0F7062FD64bb6813BeF443',
-    //   startTime: 1628813546,
-    //   endTime: 1629849941,
-    //   refundee: '0xf6401adc23Faa6B9AD83eA8604CA7254CB7F53e7',
-    // },
+    {
+      rewardToken: '0x1720668a1826c6f30a11780783b0357269b7e1ca',
+      pool: '0x1cC56b205109A832Fd0F7062FD64bb6813BeF443',
+      startTime: 1628813546,
+      endTime: 1629849941,
+      refundee: '0xf6401adc23Faa6B9AD83eA8604CA7254CB7F53e7',
+    },
     // // active until later tonight
     // {
     //   rewardToken: '0x1720668a1826c6f30a11780783b0357269b7e1ca',
@@ -41,13 +41,13 @@ export const DPIETH: V3Farm = {
     //   refundee: '0xf6401adc23Faa6B9AD83eA8604CA7254CB7F53e7',
     // },
     // // expired earlier this afternoon 4pm
-    {
-      rewardToken: '0x1720668a1826c6f30a11780783b0357269b7e1ca',
-      pool: '0x1cC56b205109A832Fd0F7062FD64bb6813BeF443',
-      startTime: 1629236400,
-      endTime: 1629236700,
-      refundee: '0xf6401adc23Faa6B9AD83eA8604CA7254CB7F53e7',
-    },
+    // {
+    //   rewardToken: '0x1720668a1826c6f30a11780783b0357269b7e1ca',
+    //   pool: '0x1cC56b205109A832Fd0F7062FD64bb6813BeF443',
+    //   startTime: 1629236400,
+    //   endTime: 1629236700,
+    //   refundee: '0xf6401adc23Faa6B9AD83eA8604CA7254CB7F53e7',
+    // },
   ],
 }
 
