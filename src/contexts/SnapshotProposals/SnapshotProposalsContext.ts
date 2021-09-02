88076e8f8e0f877bb2ad7f9be0c5ba7dfc6cc9c7
Rootul Patel <rootulp@amazon.com>
2021-09-01 21:03:02 -0400

    Remove SnapshotProposals directory
    Fixes https://github.com/SetProtocol/index-ui/issues/372
    Follow up to https://github.com/SetProtocol/index-ui/pull/367
    
    Testing
    - `yarn start` works

--- src/contexts/SnapshotProposals/SnapshotProposalsContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { SnapshotProposalsContextValues } from './types'
-
-const SnapshotProposalsContext = createContext<SnapshotProposalsContextValues>({
-  indexProposals: {},
-})
-
-export default SnapshotProposalsContext
--- src/hooks/useSnapshotProposals.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-
-import { SnapshotProposalsContext } from 'contexts/SnapshotProposals'
-
-const useSnapshotProposals = () => {
-  return { ...useContext(SnapshotProposalsContext) }
-}
-
-export default useSnapshotProposals
