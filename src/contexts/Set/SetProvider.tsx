8f6fd7a4e6dc32b0edf5b404cd0cd5f6b8bf3bbd
Rootul Patel <rootulp@amazon.com>
2021-09-12 22:25:43 -0400

    WIP: fetch quantity of DPI component from on-chain
    - Add ethers as a dep
    - Need `resolutions` block to resolve error

--- src/contexts/Set/SetProvider.tsx
+++ src/contexts/Set/SetProvider.tsx
@@ -1,20 +1,12 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import { provider } from 'web3-core'
 import Set, { SetJSConfig } from "set.js"
 
-import { basicIssuanceModuleAddress, controllerAddress, debtIssuanceModuleAddress, navIssuanceModuleAddress, protocolViewerAddress, streamingFeeModuleAddress, setTokenCreatorAddress, tradeModuleAddress, masterOracleAddress, governanceModuleAddress } from "../../constants/ethContractAddresses";
+import { basicIssuanceModuleAddress, controllerAddress, debtIssuanceModuleAddress, navIssuanceModuleAddress, protocolViewerAddress, streamingFeeModuleAddress, setTokenCreatorAddress, tradeModuleAddress, masterOracleAddress, governanceModuleAddress, dpiTokenAddress, mviTokenAddress } from "../../constants/ethContractAddresses";
 import useWallet from "../../hooks/useWallet";
 import SetContext from "./SetContext";
 
-
-
 const SetProvider: React.FC = ({ children }) => {
-  const { ethereum: web3Provider }: { ethereum: provider } = useWallet()
-  if ( web3Provider === undefined ){
-    console.error("web3Provider is not defined");
-    throw new Error("web3Provider is not defined")
-  }
-
   if (basicIssuanceModuleAddress === undefined ||
       controllerAddress === undefined ||
       masterOracleAddress === undefined ||
@@ -25,34 +17,46 @@ const SetProvider: React.FC = ({ children }) => {
       tradeModuleAddress === undefined ||
       governanceModuleAddress === undefined ||
       debtIssuanceModuleAddress === undefined) {
-      throw new Error("A set JS address is not defined")
+      throw new Error("A set JS address is not defined. Please check your .env file")
   }
 
-  const config: SetJSConfig = {
-      web3Provider: web3Provider,
-      basicIssuanceModuleAddress: basicIssuanceModuleAddress,
-      controllerAddress: controllerAddress,
-      masterOracleAddress: masterOracleAddress,
-      navIssuanceModuleAddress: navIssuanceModuleAddress,
-      protocolViewerAddress: protocolViewerAddress,
-      setTokenCreatorAddress: setTokenCreatorAddress,
-      streamingFeeModuleAddress: streamingFeeModuleAddress,
-      tradeModuleAddress: tradeModuleAddress,
-      governanceModuleAddress: governanceModuleAddress,
-      debtIssuanceModuleAddress: debtIssuanceModuleAddress,
+  const { ethereum: web3Provider }: { ethereum: provider } = useWallet()
+  if ( web3Provider === undefined ){
+    console.warn("web3Provider is not defined. Please connect to MetaMask to fetch on-chain Set data");
   }
 
-  const set = new Set(config);
+  let set: Set;
+  if (web3Provider) {
+    const config: SetJSConfig = {
+        web3Provider: web3Provider,
+        basicIssuanceModuleAddress: basicIssuanceModuleAddress,
+        controllerAddress: controllerAddress,
+        masterOracleAddress: masterOracleAddress,
+        navIssuanceModuleAddress: navIssuanceModuleAddress,
+        protocolViewerAddress: protocolViewerAddress,
+        setTokenCreatorAddress: setTokenCreatorAddress,
+        streamingFeeModuleAddress: streamingFeeModuleAddress,
+        tradeModuleAddress: tradeModuleAddress,
+        governanceModuleAddress: governanceModuleAddress,
+        debtIssuanceModuleAddress: debtIssuanceModuleAddress,
+    }
+    set = new Set(config);
+  }
 
-  function onLogSetFees() {
-      console.log("logged")
-      console.log(`set.fees`, set.fees)
+  async function onLogSetFees() {
+      // console.log("logged")
+      if (set) {
+        // const getPositions: any = await set.setToken.getPositionsAsync(dpiTokenAddress!)
+        // const getPositions: any = await set.fees.
+        // console.log(getPositions)
+        // const erc20 = getPositions[0][0]
+        // console.log(set.setToken.batchFetchSetDetailsAsync([dpiTokenAddress!, mviTokenAddress!], []))
+      }
   }
 
   return (
     <SetContext.Provider
       value={{
-        // set,
         onLogSetFees,
       }}
     >
---
+++ src/contexts/Set/getSetDetails.ts
@@ -0,0 +1,34 @@
+import { provider } from 'web3-core'
+import Set from "set.js"
+import { basicIssuanceModuleAddress, controllerAddress, debtIssuanceModuleAddress, navIssuanceModuleAddress, protocolViewerAddress, streamingFeeModuleAddress, setTokenCreatorAddress, tradeModuleAddress, masterOracleAddress, governanceModuleAddress, dpiTokenAddress, mviTokenAddress } from "../../constants/ethContractAddresses";
+import { SetDetails } from "set.js/dist/types/src/types";
+
+export async function getSetDetails(web3Provider: provider, productAddress: string): Promise<SetDetails[]> {
+  if (basicIssuanceModuleAddress === undefined ||
+      controllerAddress === undefined ||
+      masterOracleAddress === undefined ||
+      navIssuanceModuleAddress === undefined ||
+      protocolViewerAddress === undefined ||
+      setTokenCreatorAddress === undefined ||
+      streamingFeeModuleAddress === undefined ||
+      tradeModuleAddress === undefined ||
+      governanceModuleAddress === undefined ||
+      debtIssuanceModuleAddress === undefined) {
+      throw new Error("A set JS address is not defined. Please check your .env file")
+  }
+  const set = new Set({
+    web3Provider: web3Provider,
+    basicIssuanceModuleAddress: basicIssuanceModuleAddress,
+    controllerAddress: controllerAddress,
+    masterOracleAddress: masterOracleAddress,
+    navIssuanceModuleAddress: navIssuanceModuleAddress,
+    protocolViewerAddress: protocolViewerAddress,
+    setTokenCreatorAddress: setTokenCreatorAddress,
+    streamingFeeModuleAddress: streamingFeeModuleAddress,
+    tradeModuleAddress: tradeModuleAddress,
+    governanceModuleAddress: governanceModuleAddress,
+    debtIssuanceModuleAddress: debtIssuanceModuleAddress,
+  });
+
+  return set.setToken.batchFetchSetDetailsAsync([productAddress], []);
+}
