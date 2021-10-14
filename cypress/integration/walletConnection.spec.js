6dd17d69dd32116c4d0a7590fd7d5d842a963193
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-10-14 19:29:47 +0200

    Extend MockProvider and add first test cases for wallet connection button

--- cypress/integration/walletConnection.spec.js
+++ cypress/integration/walletConnection.spec.js
@@ -1,21 +1,49 @@
 /// <reference types="cypress" />
+//
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
 
 describe('Wallet Connection', () => {
-  beforeEach(() => {
-    cy.injectWeb3Provider();
-    cy.visit('http://localhost:3000/', {
-      headers: {
-        'Accept-Encoding': 'gzip, deflate',
-      },
+  context('Metamask', () => {
+    before(() => {
+      cy.injectWeb3Provider()
+      cy.visit('http://localhost:3000/', {
+        headers: {
+          'Accept-Encoding': 'gzip, deflate',
+        },
+      })
+    })
+    it('should be able to connect', () => {
+      cy.contains('Unlock Wallet').click()
+      cy.contains('Metamask').parent().parent().contains('Select').click()
+      cy.contains('View Balances')
+    })
+    it('should be able to disconnect', () => {
+      cy.contains('View Balances').parent().click()
+      cy.contains('Sign Out').click()
+      cy.contains('Unlock Wallet')
     })
   })
 
-  context('Injected Web3 Provider', () => {
-    it('should be able to connect', () => {
-      cy.get('[data-cy=home-header]').should(
-        'contain',
-        'The Index Coop currently has'
-      )
+  context('ONTO', () => {
+    before(() => {
+      cy.injectWeb3Provider(true)
+      cy.reload(true)
+    })
+    it('should connect directly', () => {
+      cy.contains('View Balances').should('be.visible')
+    })
+    it('should be able to disconnect', () => {
+      cy.contains('View Balances').parent().click()
+      cy.contains('Sign Out').click()
+      cy.contains('Unlock Wallet').should('be.visible')
+    })
+    it('should reconnect directly when clicking Unlock wallet button', () => {
+      cy.contains('Unlock Wallet').click()
+      cy.contains('View Balances').should('be.visible')
     })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,25 +2,72 @@ import { Eip1193Bridge } from '@ethersproject/experimental/lib/eip1193-bridge'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { Wallet } from '@ethersproject/wallet'
 
-const jsonRPCProviders = {
-  1: 'https://eth-mainnet.alchemyapi.io/v2/EElfXQcZk-acQCjrLv4ERIBBdCBxCY2_',
+const TEST_PRIVATE_KEY =
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const provider = new JsonRpcProvider()
+const signer = new Wallet(TEST_PRIVATE_KEY, provider)
+const TEST_ADDRESS_NEVER_USE = '0xef35B896e1c6c816177277924d04Ead587Bbc495'
+
+class MockProvider extends Eip1193Bridge {
+  chainId = 1
+
+  async sendAsync(...args) {
+    console.debug('sendAsync called', ...args)
+    return this.send(...args)
+  }
+  async send(...args) {
+    console.debug('send called', ...args)
+    const isCallbackForm =
+      typeof args[0] === 'object' && typeof args[1] === 'function'
+    let callback
+    let method
+    let params
+    if (isCallbackForm) {
+      callback = args[1]
+      method = args[0].method
+      params = args[0].params
+    } else {
+      method = args[0]
+      params = args[1]
+    }
+    if (method === 'eth_requestAccounts' || method === 'eth_accounts') {
+      if (isCallbackForm) {
+        callback({ result: [TEST_ADDRESS_NEVER_USE] })
+      } else {
+        return Promise.resolve([TEST_ADDRESS_NEVER_USE])
+      }
+    }
+    if (method === 'eth_chainId') {
+      if (isCallbackForm) {
+        callback(null, { result: '0x1' })
+      } else {
+        return Promise.resolve('0x1')
+      }
+    }
+    try {
+      const result = await super.send(method, params)
+      console.debug('result received', method, params, result)
+      if (isCallbackForm) {
+        callback(null, { result })
+      } else {
+        return result
+      }
+    } catch (error) {
+      if (isCallbackForm) {
+        callback(error, null)
+      } else {
+        throw error
+      }
+    }
+  }
 }
 
-const TEST_PRIVATE_KEY =
-  '0xe580410d7c37d26c6ad1a837bbae46bc27f9066a466fb3a66e770523b4666d19'
-
-Cypress.Commands.add(
-  'injectWeb3Provider',
-  (privateKey = TEST_PRIVATE_KEY, chainId = 1) => {
-    cy.on('window:before:load', (win) => {
-      if (jsonRPCProviders[chainId] == null) {
-        throw new Error(
-          `No json rpc provider configured for chain id ${chainId}`
-        )
-      }
-      const provider = new JsonRpcProvider(jsonRPCProviders[chainId], chainId)
-      const signer = new Wallet(privateKey, provider)
-      win.ethereum = new Eip1193Bridge(signer, provider)
-    })
-  }
-)
+Cypress.Commands.add('injectWeb3Provider', (isONTO = false) => {
+  cy.on('window:before:load', (win) => {
+    win.localStorage.clear()
+    win.ethereum = new MockProvider(signer, provider)
+    if (isONTO) {
+      win.ethereum.isONTO = isONTO
+    }
+  })
+})
