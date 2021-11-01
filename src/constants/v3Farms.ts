cfbfbae75921946e82ecac7a642fdce180e222d1
Zachary Parrish <zachary@parrish.io>
2021-11-01 17:10:20 -0400

    added ability to add MVI v3 staking

--- src/constants/v3Farms.ts
+++ src/constants/v3Farms.ts
@@ -1,6 +1,11 @@
 export interface V3Farm {
   tokenPair: string
   pool: string
+  poolLabel: string
+  img: {
+    alt: string
+    src: string
+  }
   farms: FarmData[]
 }
 
@@ -10,11 +15,21 @@ export interface FarmData {
   startTime: number
   endTime: number
   refundee: string
+  dateText: string
+  img: {
+    alt: string
+    src: string
+  }
 }
 
 export const DpiEthRewards: V3Farm = {
   tokenPair: 'DPI/ETH',
   pool: '0x9359c87B38DD25192c5f2b07b351ac91C90E6ca7',
+  poolLabel: 'DPI-ETH',
+  img: {
+    alt: 'DPI Icon',
+    src: 'https://set-core.s3.amazonaws.com/img/social_trader_set_icons/defi_pulse_index_set.svg',
+  },
   farms: [
     // August 20th - September 4th
     {
@@ -23,10 +38,40 @@ export const DpiEthRewards: V3Farm = {
       startTime: 1629493200,
       endTime: 1630789200,
       refundee: '0x9467cfADC9DE245010dF95Ec6a585A506A8ad5FC',
+      dateText: 'August 20th, 2021 - September 4th, 2021',
+      img: {
+        alt: 'DPI Icon',
+        src: 'https://set-core.s3.amazonaws.com/img/social_trader_set_icons/defi_pulse_index_set.svg',
+      },
     },
   ],
 }
 
-const farms = [DpiEthRewards]
+export const MviEthRewards: V3Farm = {
+  tokenPair: 'MVI/ETH',
+  pool: '0x9359c87B38DD25192c5f2b07b351ac91C90E6ca7',
+  poolLabel: 'MVI-ETH',
+  img: {
+    alt: 'MVI Icon',
+    src: 'https://set-core.s3.amazonaws.com/img/social_trader_set_icons/defi_pulse_index_set.svg',
+  },
+  farms: [
+    // August 20th - September 4th
+    {
+      rewardToken: '0x0954906da0Bf32d5479e25f46056d22f08464cab', //INDEX
+      pool: '0x9359c87B38DD25192c5f2b07b351ac91C90E6ca7',
+      startTime: 1629493200,
+      endTime: 1630789200,
+      refundee: '0x9467cfADC9DE245010dF95Ec6a585A506A8ad5FC', //Treasury
+      dateText: 'November 1st, 2021 - January 1st, 2022',
+      img: {
+        alt: 'DPI Icon',
+        src: 'https://set-core.s3.amazonaws.com/img/social_trader_set_icons/defi_pulse_index_set.svg',
+      },
+    },
+  ],
+}
+
+const farms = [DpiEthRewards, MviEthRewards]
 
 export default farms
