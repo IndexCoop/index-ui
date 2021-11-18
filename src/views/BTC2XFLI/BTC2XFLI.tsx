733258d8c5992aee6ce5a805b48e1f937ec28581
Dylan Tran <dylan.ad.tran@gmail.com>
2021-11-18 09:58:34 -0800

    remove product descriptions

--- src/views/BTC2XFLI/BTC2XFLI.tsx
+++ src/views/BTC2XFLI/BTC2XFLI.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { toast } from 'react-toastify'
-import ExternalLink from 'components/ExternalLink'
 
 import useBtc2xFliTokenSupplyCap from 'hooks/useBtc2xFliTokenSupplyCap'
 import useBtc2xFliTokenMarketData from 'hooks/useBtc2xFliTokenMarketData'
@@ -77,102 +76,7 @@ const Btc2xFliProductPage = (props: { title: string }) => {
     }
   }, [account, isApproachingSupplyCap])
 
-  return (
-    <ProductDataUI tokenDataProps={tokenDataProps}>
-      <strong>The Bitcoin Flexible Leverage Index</strong> lets you leverage a
-      collateralized debt position in a safe and efficient way, by abstracting
-      its management into a simple index. It enables market participants to take
-      on leverage while minimizing the transaction costs and risks associated
-      with maintaining collateralized debt.
-      <h2>Objective</h2>
-      The Bitcoin Flexible Leverage Index (BTC2X-FLI) makes leverage effortless.
-      The end user does not have to worry about:
-      <ul>
-        <li>
-          Monitoring their leveraged loan 24/7, having to always be ready to
-          act.
-        </li>
-
-        <li>
-          High fees, transactions not being included fast enough or the relative
-          UIs being unresponsive during times of high volatility.
-        </li>
-
-        <li>
-          Paying for overpriced stablecoins to deleverage on time or panic
-          trading to save their positions.
-        </li>
-      </ul>
-      BTC2X-FLI has several key advantages over Legacy Leveraged Tokens:
-      <ul>
-        <li>Zero slippage via composable entry and exit.</li>
-
-        <li>
-          Unique Index algorithm reduces rebalancing needs by an order of
-          magnitude.
-        </li>
-
-        <li>
-          Emergency deleveraging possible during Black Swan events for
-          additional fund safety.
-        </li>
-      </ul>
-      <ExternalLink
-        href='https://pulse.inc/flexible-leverage-index'
-        target='_blank'
-      >
-        <h4>View the methodology here.</h4>
-      </ExternalLink>
-      <h2>Initial Parameters:</h2>
-      <ul>
-        <li>Underlying Asset: WBTC</li>
-        <li>Target Leverage Ratio: 2</li>
-        <li>DeFi Lending Protocol: Compound</li>
-        <li>Maximum Leverage Ratio: 2.2</li>
-        <li>Minimum Leverage Ratio: 1.8</li>
-        <li>Recentering Speed: 10%</li>
-      </ul>
-      <h2>Definitions:</h2>
-      <ul>
-        <li>
-          Borrow Rate — the cost to borrow the asset at the DeFi Lending
-          Protocol over the most recent epoch.
-        </li>
-        <li>Epoch Length — the time between rebalances.</li>
-        <li>
-          Target Leverage Ratio (TLR) — the long term target for the value of
-          the assets held by the index divided by the value of the debt held by
-          the index.
-        </li>
-        <li>
-          Current Leverage Ratio (CLR) — the value of the asset currently held
-          by the index divided by the current value of the debt held by the
-          index.
-        </li>
-        <li>
-          Maximum Leverage Ratio (MAXLR) — the highest leverage ratio the index
-          will ever have after a rebalance.
-        </li>
-        <li>
-          Re-centering Speed (RS) — the rate at which the Current Leverage Ratio
-          is adjusted each period to return to the Target Leverage Ratio, when
-          the index is not being adjusted back to the Maximum Leverage Ratio or
-          the Minimum Leverage Ratio.
-        </li>
-      </ul>
-      <h3>Index Price:</h3>
-      <i>
-        FLIt = FLIt-1 * (1 + ((Pricet/Pricet-1–1) * CLRt-1 — (BorrowRatet *
-        (CLRt-1 -1)/CLRt-1)))
-      </i>
-      <h3>Calculation of the new Current Lever Ratio for the period:</h3>
-      <i>CLRt+1 = max(MINLR, min(MAXLR, CLRt * (1 - RS) + TLR * RS))</i>
-      <h2>Fees:</h2>
-      Flexible Leverage Index has a streaming fee of 1.95% (195 basis points)
-      and a 0.1% (10 basis points) mint/redeem fee. The revenue generated from
-      the streaming fee is split 40% to DeFi Pulse and 60% to Index Coop.
-    </ProductDataUI>
-  )
+  return <ProductDataUI tokenDataProps={tokenDataProps} />
 }
 
 export default Btc2xFliProductPage
